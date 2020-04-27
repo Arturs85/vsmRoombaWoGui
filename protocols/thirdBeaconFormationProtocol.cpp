da16200869961754fcf6b8a29f2f725a4635f610
arturs <arturs.ardavs@gmail.com>
2020-04-27 23:25:29 +0300

    started to implement thirdbeaconfp

--- protocols/thirdBeaconFormationProtocol.cpp
+++ protocols/thirdBeaconFormationProtocol.cpp
@@ -1,3 +1,4 @@
+#include "thirdBeaconFormationProtocol.hpp"
 #include "twoPointFormationProtocol.hpp"
 #include "vSMMessage.hpp"
 #include "roombaAgent.hpp"// for editing
@@ -10,7 +11,7 @@
 #define BEACONS_TRIANGLE_SIDE_MM 1500
 
 #define PI 3.1415926
-bool TwoPointFormationProtocol::tick()
+bool ThirdBeaconFormationProtocol::tick()
 {
     //cout<<"tpf tick \n";
     switch (roleInProtocol) {
@@ -27,14 +28,14 @@ bool TwoPointFormationProtocol::tick()
     return false;
 }
 
-void TwoPointFormationProtocol::start()
+void ThirdBeaconFormationProtocol::start()
 {
     switch (roleInProtocol) {
     case RoleInProtocol::STANDING_BEACON:// send query for confirmation of protocol start to still beacon
         state = ProtocolStates::WAITING_START_REQUEST;
         break;
     case RoleInProtocol::MOVING_BEACON:{
-        VSMMessage startRequest(behaviour->owner->id,Topics::TWO_POINT_FORMATION_TO_STILL,MessageContents::TPF_START,"r");
+        VSMMessage startRequest(behaviour->owner->id,Topics::THIRD_BEACON_OUT,MessageContents::TPF_START,"r");
         behaviour->owner->sendMsg(startRequest);
         state = ProtocolStates::WAITING_REPLY;
     }
@@ -44,7 +45,7 @@ void TwoPointFormationProtocol::start()
 }
 
 
-bool TwoPointFormationProtocol::standingBeaconTick()
+bool ThirdBeaconFormationProtocol::standingBeaconTick()
 {
     //wait for request from movingBeacon to start, then stay in still mode
     switch (state) {
@@ -68,24 +69,37 @@ bool TwoPointFormationProtocol::standingBeaconTick()
     }
     return false;
 }
+
 //returns true if protocol has ended
-bool TwoPointFormationProtocol::movingBeaconTick()
+bool ThirdBeaconFormationProtocol::movingBeaconTick()
 {
     switch (state) {
     case ProtocolStates::WAITING_REPLY:{
         VSMMessage* res = behaviour->receive(MessageContents::AGREE);
         if(res!= 0){
-            stillBeaconId=res->senderNumber;// read other beacon id from its reply, id will be needed in dist measurement
-            startDistanceMeasurement(stillBeaconId,ProtocolStates::WAITING_REPLY,ProtocolStates::TIMEOUT);// return to this same state after measurement is received or timeout state if not
+           stillBeaconResponders.insert(res->senderNumber);// read other beacon id from its reply, id will be needed in dist measurement
+           if(stillBeaconResponders.size()>=2){
+               stillBeacon1Id = *(stillBeaconResponders.begin());
+               stillBeacon2Id = *(stillBeaconResponders.begin()+1);
+               startDistanceMeasurement(stillBeacon1Id,ProtocolStates::FIRST_MEASUREMENT_RECEIVED_FROM_1,ProtocolStates::TIMEOUT);// return to this same state after measurement is received or timeout state if not
+
+           }
 
         }
-        if(latestMeasurement!=0){//measurement result is received
-            measuredDist[0]= latestMeasurement;
 
-            enterState(ProtocolStates::FIRST_MOVE);
-        }
     }
         break;
+
+    case ProtocolStates::FIRST_MEASUREMENT_RECEIVED_FROM_1:
+         measuredDistToFirst[0]= latestMeasurement;//
+         startDistanceMeasurement(stillBeacon2Id,ProtocolStates::FIRST_MEASUREMENT_RECEIVED_FROM_2,ProtocolStates::TIMEOUT);// return to this same state after measurement is received or timeout state if not
+
+        break;
+    case ProtocolStates::FIRST_MEASUREMENT_RECEIVED_FROM_2:
+         measuredDistToSecond[0]= latestMeasurement;//
+enterState(ProtocolStates::FIRST_MOVE);
+        break;
+
     case ProtocolStates::WAITING_DIST_MEASURE_RESULT:{
         measureWaitCounter++;
         VSMMessage* res = behaviour->receive(MessageContents::DISTANCE_MEASUREMENT);
@@ -101,16 +115,16 @@ bool TwoPointFormationProtocol::movingBeaconTick()
         break;
     case ProtocolStates::FIRST_MOVE:
         if(behaviour->owner->movementManager->state==MovementStates::FINISHED){// movement is done
-            startDistanceMeasurement(stillBeaconId,ProtocolStates::FIRST_MOVE_MEASUREMENT_RECEIVED,ProtocolStates::TIMEOUT);
-            std::cout<<">> tpfp started second measurement\n";
+            startDistanceMeasurement(stillBeacon1Id,ProtocolStates::FIRST_MOVE_MEASUREMENT_RECEIVED,ProtocolStates::TIMEOUT);
+            std::cout<<">> 3rdbfp started second measurement\n";
         }break;
     case ProtocolStates::FIRST_MOVE_MEASUREMENT_RECEIVED:// now we have two measurements and traveled distance
-        measuredDist[1]=latestMeasurement;
+        measuredDistToFirst[1]=latestMeasurement;
         relativeAngleH1 = calculateRelativeAngle(measuredDist[0], measuredDist[1], TRIANGLE_SIDE_MM);
         relativeAngleH2 = -calculateRelativeAngle(measuredDist[0], measuredDist[1], TRIANGLE_SIDE_MM);
         
         std::cout<<"relAngleH1 "<<relativeAngleH1<<"  relAngleH2 "<<relativeAngleH2<<"\n";
-        
+       // todo -continue from here- measure dist to second beacon
         dirBeforeTurn = behaviour->owner->movementManager->direction;
         behaviour->owner->movementManager->turn(relativeAngleH1*180/PI);//todo turn right is in sim, implement in movement manager
 
@@ -215,7 +229,7 @@ bool TwoPointFormationProtocol::movingBeaconTick()
     return false;
 }
 
-void TwoPointFormationProtocol::enterState(ProtocolStates stateToEnter)
+void ThirdBeaconFormationProtocol::enterState(ProtocolStates stateToEnter)
 {
     switch (stateToEnter) {
 
@@ -223,25 +237,25 @@ void TwoPointFormationProtocol::enterState(ProtocolStates stateToEnter)
         // start to move robot: owner.publicPartOfAgent.moveForwardBy(triangleTravelside);
         behaviour->owner->movementManager->driveDistance(TRIANGLE_SIDE_MM);  // drive forward distace
         state = ProtocolStates::FIRST_MOVE;
-        cout<<("entering first move\n");
+        cout<<("3rdbfp entering first move\n");
         break;
 
     case ProtocolStates::SECOND_MOVE:
         // start to move robot: owner.publicPartOfAgent.moveForwardBy(triangleTravelside);
         behaviour->owner->movementManager->driveDistance(TRIANGLE_SIDE_MM/2);  // drive forward distace
         state = ProtocolStates::SECOND_MOVE;
-        cout<<("entering second move\n");
+        cout<<("3rdbfp entering second move\n");
         break;
 
     case ProtocolStates::FINAL_POSITION_MOVE:
         // start to move robot: owner.publicPartOfAgent.moveForwardBy(triangleTravelside);
         state = ProtocolStates::FINAL_POSITION_MOVE;
-        cout<<("entering final move\n");
+        cout<<("3rdbfp entering final move\n");
         break;
     }
 }
 
-void TwoPointFormationProtocol::startDistanceMeasurement(int id,ProtocolStates nextStatePosResult,ProtocolStates nextStateNegResult)
+void ThirdBeaconFormationProtocol::startDistanceMeasurement(int id,ProtocolStates nextStatePosResult,ProtocolStates nextStateNegResult)
 {
     latestMeasurement=0;
     nextStateOnNegativeResult = nextStateNegResult;// assign these values directly prior this call?
@@ -251,7 +265,7 @@ void TwoPointFormationProtocol::startDistanceMeasurement(int id,ProtocolStates n
 }
 
 
-TwoPointFormationProtocol::TwoPointFormationProtocol(RoleInProtocol roleInProtocol, BaseCommunicationBehaviour *ownerBeh):BaseProtocol(ownerBeh)
+ThirdBeaconFormationProtocol::ThirdBeaconFormationProtocol(RoleInProtocol roleInProtocol, BaseCommunicationBehaviour *ownerBeh):BaseProtocol(ownerBeh)
 {
     this->roleInProtocol=roleInProtocol;
     if(roleInProtocol==RoleInProtocol::STANDING_BEACON)// responder needs to listen for requests in this topic, initiator will receive direct messages
@@ -264,25 +278,4 @@ TwoPointFormationProtocol::TwoPointFormationProtocol(RoleInProtocol roleInProtoc
 
 
 
-double TwoPointFormationProtocol::calculateRelativeAngle(double mes1, double mes2, double odoDist)
-{
-    if ((mes2 + odoDist) < mes1) return 0;
-    if ((mes1 + odoDist) < mes2) return PI;// todo revisit
-    double angle = acos((odoDist * odoDist + mes2 * mes2 - mes1 * mes1) / (2 * odoDist * mes2));
-    std::cout<<"calcRelAngle called "<<mes1<<" "<<mes2<<" "<<odoDist<<" angle: "<<angle<<"\n";
 
-    return PI - angle;
-}
-double TwoPointFormationProtocol::calcThirdSide(double a,double b,double angleRad){
-    return sqrt(a*a+b*b-2*a*b*cos(angleRad));
-}
-
-double TwoPointFormationProtocol::calcAngle(double a, double b, double c)
-{ std::cout<<"calcAngle called "<<a<<" "<<b<<" "<<c<<"\n";
-    if ((a + b) < c) return PI;
-    if (abs(a-b) > c) return 0;
-    //cos theorem
-    return acos((a*a+b*b-c*c)/(2*a*b));
-
-
-}
---
+++ protocols/thirdBeaconFormationProtocol.hpp
@@ -0,0 +1,54 @@
+#ifndef THIRDBEACONFORMATIONPROTOCOL_HPP
+#define THIRDBEACONFORMATIONPROTOCOL_HPP
+#include "baseProtocol.hpp"
+#include <set>
+//enum ProtocolStates{STARTED, FINISHED,WAITING_REPLY};
+
+class ThirdBeaconFormationProtocol: public BaseProtocol {
+public:
+    //roleInProtocol::Responder is only usable for S3Behaviour, pointer to ownerBehaviour is cast to S3beh
+ThirdBeaconFormationProtocol(RoleInProtocol roleInProtocol, BaseCommunicationBehaviour *ownerBeh);
+
+int stillBeacon1Id=0;
+int stillBeacon2Id=0;
+
+virtual void start();
+virtual bool tick();
+
+std::set<int> stillBeaconResponders;
+static constexpr int measureRetries = 3;
+static constexpr int measureResWaitTicks = 2;
+
+
+private:
+int measureRetryCounter=0;
+int measureWaitCounter=0;
+
+int measuredDistToFirst[4];
+int measuredDistToSecond[4];
+
+int latestMeasurement=0;
+
+double relativeAngleH1First;
+double relativeAngleH2First;
+double relativeAngleH1Second;
+double relativeAngleH2Second;
+
+double dirBeforeTurn;
+   double angleToFirstRobot;
+   double angleToSecondRobot;
+
+ProtocolStates nextStateOnPositeiveResult;
+ProtocolStates nextStateOnNegativeResult;
+bool standingBeaconTick();
+bool movingBeaconTick();
+void enterState(ProtocolStates stateToEnter);// for moving beacon
+void startDistanceMeasurement(int id, ProtocolStates nextStatePosResult, ProtocolStates nextStateNegResult);
+
+};
+
+
+
+
+
+#endif //THIRDBEACONFORMATIONPROTOCOL_HPP
